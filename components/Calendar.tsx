fe58b95ed00effef05091c0f94433d7e2ff9a1f7
HabibBehdarvandiDev <habibbehdarvandi.dev@gmail.com>
2024-06-05 21:53:11 +0330

    install jalali-moment

---
+++ components/Calendar.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useState } from "react";
+import moment, { Moment } from "moment-jalaali";
+import { Button } from "./ui/button";
+import { ChevronDown, ChevronUp } from "lucide-react";
+
+const Calendar = () => {
+  return <div>Calendar</div>;
+};
+
+export default Calendar;
